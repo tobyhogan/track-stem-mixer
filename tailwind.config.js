fd60bb3d3f275e5034884f94259c24be4f075eca
Toby Hogan <53061712+KrypticSilver@users.noreply.github.com>
2023-09-16 00:49:32 +0100

    added tailwind correctly

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,16 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-
-
 module.exports = {
-  content: [
-    "./src/**/*.html",
-    "./src/**/*.js",
-    "./src/*.js",
-    "./src/*.html",
-  ],
+  content: ["./src/**/*.{html,js}"],
   theme: {
     extend: {},
   },
   plugins: [],
 }
-
