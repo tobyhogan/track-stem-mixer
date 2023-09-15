c6a5ac13d71451239b3e0358b63a6ef54422cfad
Toby Hogan <53061712+KrypticSilver@users.noreply.github.com>
2023-09-15 20:57:56 +0100

    started working on button to play all

---
+++ index.js
@@ -0,0 +1,13 @@
+console.log("working00");
+
+const elements = document.getElementsByClassName('track');
+
+console.log("elements length: " + toString(elements.length))
+
+for(let i = 0; i < 5; i++) {
+    console.log("hello world");
+
+    console.log(elements[i]);
+}
+
+console.log(elements);
