61ea55efabdef2159ee6cd5caba0a162deb63851
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2023-09-16 08:04:43 +0100

    got audio working in next

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,15 +1,97 @@
-import Image from 'next/image'
 import Track from './components/track.tsx';
 
-import music from '../assets/music';
 
 export default function Home() {
+
+  console.log("working00");
+
+  /*
+
+
+
+
+var musicDir = "/assets/music"
+var currentSong = "sweep-me-off-my-feet";
+
+function playAllAudio() {
+
+    const elements = document.querySelectorAll(".track");
+
+
+    for(let i = 0; i < elements.length; i++) {
+
+    
+        const audioElement = elements[i].querySelector("audio");
+
+        console.log(audioElement);
+
+        audioElement.play();
+    
+    
+    
+    }    
+}
+
+function pauseAllAudio() {
+
+    const elements = document.querySelectorAll(".track");
+
+
+    for(let i = 0; i < elements.length; i++) {
+
+    
+        const audioElement = elements[i].querySelector("audio");
+
+        console.log(audioElement);
+
+        audioElement.pause();
+    
+    
+    
+    }    
+}
+
+function stopAllAudio() {
+
+    const elements = document.querySelectorAll(".track");
+
+
+    for(let i = 0; i < elements.length; i++) {
+
+    
+        const audioElement = elements[i].querySelector("audio");
+
+        console.log(audioElement);
+
+        audioElement.pause();
+        audioElement.currentTime = 0;
+    
+    
+    
+    }    
+}
+
+var slider = document.getElementById("volume-knob");
+
+slider.addEventListener("input", (event) => {console.log(event.target.value); })
+
+console.log(slider.value)
+
+*/
+
 
   function myFunc() {
 
     console.log("hello world");
 
+
+
   }
+
+  const musicFolder = "./assets/music"
+  const currentSongFolder = "/sweep-me-off-my-feet/"
+
+  console.log(process.env.PUBLIC_URL);
 
   return (
     <>
@@ -21,7 +103,12 @@ export default function Home() {
         <button onClick={myFunc()}>Pause All</button>
         <button onClick={myFunc()}>Stop All</button>
 
-        <Track source={"../public/assets/music/sweep-me-off-my-feet/pond-sweepme-bass.mp3"}></Track>
+        <Track source={musicFolder + currentSongFolder + "pond-sweepme-lead.mp3"}></Track>
+        <Track source={musicFolder + currentSongFolder + "pond-sweepme-strings.mp3"}></Track>
+        <Track source={musicFolder + currentSongFolder + "pond-sweepme-bass.mp3"}></Track>
+        <Track source={musicFolder + currentSongFolder + "pond-sweepme-brass.mp3"}></Track>
+        <Track source={musicFolder + currentSongFolder + "pond-sweepme-glock.mp3"}></Track>
+        <Track source={musicFolder + currentSongFolder + "sweep-me-off-my-feet-drums-v3.mp3"}></Track>
 
    
 
