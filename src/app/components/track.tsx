017bebbec60d0f9881646cbee40451e267340084
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2023-09-16 16:38:28 +0100

    got slider event handler working properly

--- src/app/components/track.tsx
+++ src/app/components/track.tsx
@@ -1,42 +1,46 @@
 'use client';
 
-import React, { useState } from "react";
+import React, { useEffect, useState, useRef } from "react";
 
 
 const Track = ({source}) => {
 
     const [val, setVal] = useState(84);
 
+    const inputRef = useRef(null);
+
     function myFunc(event) {
 
 
-        console.log(event);
-        console.log(event.target.value);
-        console.log("work");
 
-        setAmount(event.target.value);
+        console.log(event.target.value);
+
     }
+
+    useEffect(() => {
+        inputRef.current.addEventListener("input", myFunc);
+    }, []);
     
 
-  return (
+    return (
 
 
-    <div className="track flex">
+        <div className="track flex">
 
-        <audio controls controlsList="nodownload noplaybackrate">
-            Your browser does not support the audio element.
-            <source src={source} type="audio/mp3">
-            </source>
-        </audio>
+            <audio controls controlsList="nodownload noplaybackrate">
+                Your browser does not support the audio element.
+                <source src={source} type="audio/mp3">
+                </source>
+            </audio>
 
-        <div className="flex audio-controls w-32 h-15 bg-white rounded-lg ml-2">
-            <input onChange={myFunc} type="range" id="volume-knob" min="0" max="100" value={val} data-diameter='50' data-bgcolor="#a8a7b5" data-fgcolor="#4a4a4a" className="input-knob volume-knob outline-none focus:outline-none outline-transparent border-transparent focus:ring-0"/>
-            
+            <div className="flex audio-controls w-32 h-15 bg-white rounded-lg ml-2">
+                <input onChange={myFunc} ref={inputRef} type="range" id="volume-knob" min="0" max="100" value={val} data-diameter='50' data-bgcolor="#a8a7b5" data-fgcolor="#4a4a4a" className="input-knob volume-knob outline-none focus:outline-none outline-transparent border-transparent focus:ring-0"/>
+                
+            </div>
+
         </div>
 
-    </div>
-
-  );
+    );
 };
 
 export default Track;
