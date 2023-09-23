decc106240da2ac2f00f74b84c154f4377053c12
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2023-09-23 20:23:58 +0100

    .

--- src/components/Track.js
+++ src/components/Track.js
@@ -1,24 +1,11 @@
-class Track extends HTMLElement {
+class Track extends HTMLDivElement  {
     constructor() {
       super();
     }
   
     connectedCallback() {
-      this.innerHTML = `
 
-
-      <audio controls controlsList="nodownload noplaybackrate">
-      Your browser does not support the audio element.
-          <source src="./assets/pond-sweepme-lead.mp3" type="audio/mp3">
-      </audio>
-  
-      <div class="flex audio-controls w-32 h-15 bg-white rounded-lg ml-2">
-          <input id="volume-knob" type="range" value="100" class="input-knob volume-knob" outline-none focus:outline-none outline-transparent border-transparent focus:ring-0 min="0" max="100" data-diameter="40" data-fgcolor="#404040" data-bgcolor="#9da4b0"/>
-      </div>
-
-
-      `;
     }
   }
   
-  customElements.define('track-component', Track);
+customElements.define('track-component', Track, { extends: "div" });
--- src/index.js
+++ src/index.js
@@ -1,23 +1,93 @@
 
-function changeFunc() {
 
-    //assuming sweep me off my feet
+
+const songsData = {
+    "sweep-me-off-my-feet": [
+        "pond-sweepme-bass.mp3",
+        "pond-sweepme-brass.mp3",
+        "pond-sweepme-glock.mp3",
+        "pond-sweepme-lead.mp3",
+        "pond-sweepme-strings.mp3",
+        "sweep-me-off-my-feet-drums-v4.mp3"
+    ],
+    "cause-im-a-man":[
+        "tame-cause-im-a-man_bells.mp3",
+        "tame-cause-im-a-man_choir.mp3",
+        "tame-cause-im-a-man_lead.mp3",
+        "tame-cause-im-a-man_pad_solo.mp3",
+        "tame-cause-im-a-man_strings.mp3"
+    ]
+}
+
+
+
+
+
+
+function updateSong() {
+
+
+
+
+
 
 
     var selectBox = document.getElementById("song-select");
     var selectedValue = selectBox.options[selectBox.selectedIndex].value;
 
+    tag = document.getElementById("viewer-widget");
+
+    for (var key in songsData) {
+
+
+        if (selectedValue == key) {
+
+
+
+            for (var count in songsData[key]) {
+
+
+                path = "assets/songs/" + selectedValue + "/" + songsData[key][count]
+
+
+
+                let elem = document.createElement("div", { is: "track-component"});
+
+                elem.setAttribute("class", "flex");
+
+                elem.innerHTML = `
+
+
+                <audio controls controlsList="nodownload noplaybackrate" src="${path}">
+                Your browser does not support the audio element.
+                </audio>
+          
+                <div class="flex audio-controls w-32 h-15 bg-white rounded-lg ml-2">
+                    <input id="volume-knob" type="range" value="100" class="input-knob volume-knob" outline-none focus:outline-none outline-transparent border-transparent focus:ring-0 min="0" max="100" data-diameter="40" data-fgcolor="#404040" data-bgcolor="#9da4b0"/>
+                </div>
+          
+          
+                `;
+
+
+                tag.append(elem);
+
+                
+
+            }
+
+
+
+
+        }
+
+
+    }
+
+  
 
     tag = document.getElementById("viewer-widget");
 
-    var audio1 = document.createElement("AUDIO");
-    audio1.setAttribute("src", "assets/pond-sweepme-bass.mp3");
-    audio1.setAttribute("controls", "controls");
-    audio1.classList.add("track");
-
-
-    tag.appendChild(audio1);
-  
 
 
 
