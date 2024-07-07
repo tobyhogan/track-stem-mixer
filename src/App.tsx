fed245a04bad26086e499c5d2898de57c3eea086
Toby Hogan <53061712+tobyhogan@users.noreply.github.com>
2024-07-07 18:20:54 +0100

    .

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,23 @@ function App() {
 
   return (
     <>
-      <h1 className='bg-blue-500 text-5xl mx-auto mt-5 px-10 py-4 w-fit'>Test.</h1>
+      <div className=''>
+        <div className='w-[32rem] h-[34rem] border-2 border-black mx-auto mt-16'>
+          <h1 className='mt-3 text-2xl text-center underline'>Music Viewer</h1>
+          <div className='bg-grayNew-150 flex justify-around w-fit px-6 py-2 mt-8 ml-6 [&>*]:mr-3'>
+            <button className='bg-grayNew-250 px-4 py-1 text-grayNew-700'>Play</button>
+            <button className='bg-grayNew-250 px-4 py-1 text-grayNew-700'>Play</button>
+            <button className='bg-grayNew-250 px-4 py-1 text-grayNew-700'>Play</button>
+          </div>
+          <h2>Song:</h2>
+          <h2>Components:</h2>
+          <div className='bg-grayNew-150 h-'>
+            <div className='py-3'>Component1</div>
+            <div className='py-3'>Component2</div>
+            <div className='py-3'>Component3</div>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
